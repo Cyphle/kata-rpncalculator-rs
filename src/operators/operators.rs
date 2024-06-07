2b4796e3d664ae6e3625fb353c255177904082c9
Cyphle <cyril.phamle@gmail.com>
2024-06-07 08:44:25 +0200

    operators

--- src/operators/operators.rs
+++ src/operators/operators.rs
@@ -3,16 +3,23 @@ enum Operators {
     PLUS,
     MINUS,
     MULTIPLY,
-    SUBTRACT
+    DIVIDE
 }
 
 fn find_operator_from(symbol: &char) -> Option<Operators> {
-    None
+    match symbol {
+        &'+' => Some(Operators::PLUS),
+        &'-' => Some(Operators::MINUS),
+        &'*' => Some(Operators::MULTIPLY),
+        &'/' => Some(Operators::DIVIDE),
+        _ => None
+    }
 }
 
 #[cfg(test)]
 mod find_operator_from_tests {
-    use crate::operators::operators::{find_operator_from, Operators};
+    use crate::operators::operators::find_operator_from;
+    use crate::operators::operators::Operators::{DIVIDE, MINUS, MULTIPLY, PLUS};
 
     #[test]
     fn should_return_unknown_when_char_is_not_an_operator() {
@@ -21,5 +28,41 @@ mod find_operator_from_tests {
         let result = find_operator_from(&symbol);
 
         assert_eq!(result, None);
+    }
+
+    #[test]
+    fn should_return_plus_when_char_is_plus_operator() {
+        let symbol = '+';
+
+        let result = find_operator_from(&symbol);
+
+        assert_eq!(result, Some(PLUS));
+    }
+
+    #[test]
+    fn should_return_minus_when_char_is_minus_operator() {
+        let symbol = '-';
+
+        let result = find_operator_from(&symbol);
+
+        assert_eq!(result, Some(MINUS));
+    }
+
+    #[test]
+    fn should_return_multiply_when_char_is_multiply_operator() {
+        let symbol = '*';
+
+        let result = find_operator_from(&symbol);
+
+        assert_eq!(result, Some(MULTIPLY));
+    }
+
+    #[test]
+    fn should_return_divide_when_char_is_divide_operator() {
+        let symbol = '/';
+
+        let result = find_operator_from(&symbol);
+
+        assert_eq!(result, Some(DIVIDE));
     }
 }
