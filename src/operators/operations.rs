9f5f3f412b95dedb33d59d777cc6d16d7df03367
Cyril Pham-le <cyril.pham-le@stonal.com>
2024-06-11 16:10:38 +0200

    operation

--- src/operators/operations.rs
+++ src/operators/operations.rs
@@ -1,6 +1,30 @@
-// pub struct Operation {
-//     operand: String,
-//     operand: String,
-// }
-//
-// pub fn build_operations_from(instructions: Vec<char>) ->
+use crate::operators::Operators;
+
+pub struct Operation {
+    left_operand: String,
+    right_operand: String,
+    operator: Operators
+}
+
+pub fn build_operations_from(instructions: &Vec<String>) -> Operation {
+
+}
+
+#[cfg(test)]
+mod build_operations_from_tests {
+    use crate::operators::operations::{build_operations_from, Operation};
+    use crate::operators::Operators;
+
+    #[test]
+    fn should_build_an_operation_from_simple_instructions() {
+        let instructions = vec!["5".to_string(), "3".to_string(), "+".to_string()];
+
+        let result = build_operations_from(&instructions);
+
+        assert_eq!(result, Operation {
+            left_operand: "5".to_string(),
+            right_operand: "3".to_string(),
+            operator: Operators::PLUS
+        })
+    }
+}
