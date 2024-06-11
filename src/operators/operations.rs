2ff680c209fcb6a73e5653afa9f86e165c3fb541
Cyril Pham-le <cyril.pham-le@stonal.com>
2024-06-11 17:56:36 +0200

    should_build_an_operation_from_simple_instructions

--- src/operators/operations.rs
+++ src/operators/operations.rs
@@ -1,13 +1,31 @@
-use crate::operators::Operators;
+use crate::operators::{find_operator_from, Operators};
 
+#[derive(Debug, PartialEq)]
 pub struct Operation {
-    left_operand: String,
-    right_operand: String,
+    operands: Vec<String>,
     operator: Operators
 }
 
 pub fn build_operations_from(instructions: &Vec<String>) -> Operation {
+    let mut operands: Vec<String> = vec![];
+    let mut operator: Operators = Operators::PLUS;
+    for i in instructions {
+        if i.len() > 1 {
+            operands.push(i.clone());
+        } else {
+            let i_as_char = i.chars().nth(0).unwrap();
+            let op = find_operator_from(&i_as_char);
+            match op {
+                None => { operands.push(i.clone())}
+                Some(o) => { operator = o }
+            }
+        }
+    }
 
+    Operation {
+        operands,
+        operator
+    }
 }
 
 #[cfg(test)]
@@ -22,8 +40,7 @@ mod build_operations_from_tests {
         let result = build_operations_from(&instructions);
 
         assert_eq!(result, Operation {
-            left_operand: "5".to_string(),
-            right_operand: "3".to_string(),
+            operands: vec!["5".to_string(), "3".to_string()],
             operator: Operators::PLUS
         })
     }
