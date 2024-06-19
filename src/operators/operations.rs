b7ba1e5f4f477c3d3dffb8935115040e98a35f43
Cyril Pham-le <cyril.pham-le@stonal.com>
2024-06-19 14:03:59 +0200

    should_build_an_operation_when_two_operations

--- src/operators/operations.rs
+++ src/operators/operations.rs
@@ -9,6 +9,7 @@ pub struct Operation {
 pub fn build_operations_from(instructions: &Vec<String>) -> Operation {
     let mut operands: Vec<String> = vec![];
     let mut operator: Operators = Operators::PLUS;
+
     for i in instructions {
         if i.len() > 1 {
             operands.push(i.clone());
@@ -35,6 +36,7 @@ mod build_operations_from_tests {
 
     #[test]
     fn should_build_an_operation_from_simple_instructions() {
+        // 5 3 + => 5+3 => 8
         let instructions = vec!["5".to_string(), "3".to_string(), "+".to_string()];
 
         let result = build_operations_from(&instructions);
@@ -43,5 +45,34 @@ mod build_operations_from_tests {
             operands: vec!["5".to_string(), "3".to_string()],
             operator: Operators::PLUS
         })
+    }
+
+    #[test]
+    fn should_build_an_operation_from_simple_instructions_when_divide() {
+        // 6 2 / => 6/2 => 3
+        let instructions = vec!["6".to_string(), "2".to_string(), "/".to_string()];
+
+        let result = build_operations_from(&instructions);
+
+        assert_eq!(result, Operation {
+            operands: vec!["6".to_string(), "2".to_string()],
+            operator: Operators::DIVIDE
+        })
+    }
+
+    #[test]
+    fn should_build_an_operation_when_two_operations() {
+        // 5 2 - 7 + => (5 2 -) 7 + => (5 - 2) + 7 => 10
+        /*
+        struct Operation {
+            left_operand: Operation,
+            right_operand: Option<Operation>,
+            operator: Operators
+        }
+
+        Il faudrait un Operators::Identity
+        et il faudrait walk through vec
+
+         */
     }
 }
